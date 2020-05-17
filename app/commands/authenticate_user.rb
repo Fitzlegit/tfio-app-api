c8c7dcebac35b8dd793069173f581eea659abdb6
Jacob Fitzhugh <j.rfitzhugh@gmail.com>
2020-05-17 19:57:16 -0400

    added authenticate user command

---
+++ app/commands/authenticate_user.rb
@@ -0,0 +1,29 @@
+#Command takes the user's email and password then returns the user, if
+# the credentials match
+
+class AuthenticateUser
+  prepend SimpleCommand
+
+  def initialize(email, password)
+    #this is where parameters are taken when the command is called
+    @email = email
+    @password = password
+  end
+
+  def call
+    #this is where the result gets returned
+    JsonWebToken.encode(user_id: user.id) if user
+  end
+
+  private
+
+  attr_accessor :email, :password
+
+  def user
+    user = User.find_by_email(email)
+    return user if user && user.authenticate(password)
+
+    errors.add :user_authentication, 'invalid credentials'
+    nil
+  end
+end
