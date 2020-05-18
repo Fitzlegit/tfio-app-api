70ecf0180022201265547fc6c18b4abf4e0bad99
Jacob Fitzhugh <j.rfitzhugh@gmail.com>
2020-05-17 20:12:41 -0400

    added comments

--- app/commands/authorize_api_request.rb
+++ app/commands/authorize_api_request.rb
@@ -15,15 +15,21 @@ class AuthorizeApiRequest
 
   attr_reader :headers
 
+  #
   def user
+    # returns nil if user isn't found or false
     @user ||= User.find(decode_auth_token[:user_id]) if decoded_auth_token
+    # returns implicitly a user object or throws error
     @user || errors.add(:tokens, 'invalid token') && nil
   end
 
+  # decodes the token received from http_auth_header
   def decoded_auth_token
     @decode_auth_token ||= JsonWebToken.decode(http_auth_header)
   end
 
+
+  # extracts token from authorization header in initialization
   def http_auth_header
     if headers['Authorization'].present?
       return headers['Authorization'].split(' ').last
