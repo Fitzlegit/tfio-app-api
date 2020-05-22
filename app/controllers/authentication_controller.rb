07beb23d252b02dbcea837e9823461449dfd9c9a
Jacob Fitzhugh <j.rfitzhugh@gmail.com>
2020-05-22 09:30:05 -0400

    fixed typo causing server error 500

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,11 +1,13 @@
 class AuthenticationController < ApplicationController
   skip_before_action :authenticate_request
 
+  # calls the authicate user class and assigns it to command
   def authenticate
     command = AuthenticateUser.call(params[:email], params[:password])
 
+    # checks command is false or true authorizes or rejects request
     if command.success?
-      render json: {auth_token: command. result }
+      render json: {auth_token: command.result }
     else
       render json: { error: command.errors }, status: :unauthrized
     end
